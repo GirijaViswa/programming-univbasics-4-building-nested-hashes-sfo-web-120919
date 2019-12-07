11062366a751533f412fcadc149ad9a9b62cfba4
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-07 03:52:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  Hash = {"Canada" => {:capital => "Ottawa", :capital_climate => "Kőppen Dfb"}
+  Hash = {"Canada" => {:capital => "Ottawa", :capital_climate => "Kőppen Dfb"}}
 end
 
 def monopoly_with_second_tier
